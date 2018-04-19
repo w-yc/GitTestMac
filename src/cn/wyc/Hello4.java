e558c36de1b04907aaeb6146cfcbdb7a940d0727
w-yc <503344350@qq.com>
2018-04-19 23:53:43 +0800

    MacTest4 添加Hello5.java

--- src/cn/wyc/Hello4.java
+++ src/cn/wyc/Hello4.java
@@ -1,4 +1,5 @@
 package cn.wyc;
 
 public class Hello4 {
+
 }
---
+++ src/cn/wyc/Hello5.java
@@ -0,0 +1,7 @@
+package cn.wyc;
+
+public class Hello5 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
